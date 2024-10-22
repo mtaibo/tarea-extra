c7aff670582705b017b0bfa8523bd8ed8306f774
mtaibo <118902623+mtaibo@users.noreply.github.com>
2024-10-22 12:29:18 +0200

    Algoritmo 7

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# Algoritmo 7
 
+months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
+          'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 
+          'Diciembre']
+
+while True:
+    month = int(input('Introduce el número del mes: '))
+    if 0 <= month or month >= 12: print('No es un mes válido.')
+    else: print(f'El mes {month} es {months[month - 1]}')
+
+
+# Algoritmo 8 
